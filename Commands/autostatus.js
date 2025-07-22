cca3fb9995afd978105dfa2f53de7bbf1cb94ba0
lyrical54 <njo3690123@gmail.com>
2025-07-22 11:39:01 +0300

    Update autostatus.js

--- Commands/autostatus.js
+++ Commands/autostatus.js
@@ -7,7 +7,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363348739987203@newsletter',
-            newsletterName: 'Arslan-Ai',
+            newsletterName: 'lyrical-Ai',
             serverMessageId: -1
         }
     }
