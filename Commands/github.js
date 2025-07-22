543fc8e17f64ff8dd60b4edc61b3f5e1ab576792
lyrical54 <njo3690123@gmail.com>
2025-07-22 11:44:12 +0300

    Update github.js

--- Commands/github.js
+++ Commands/github.js
@@ -6,11 +6,11 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/Arslan-MD/Arslan-Ai');
+    const res = await fetch('https://github.com/lyrical54/LYRICAL-XMD');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*乂  Arslan-Ai  乂*\n\n`;
+    let txt = `*乂 🖕  乂*\n\n`;
     txt += `✩  *Name* : ${json.name}\n`;
     txt += `✩  *Watchers* : ${json.watchers_count}\n`;
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
@@ -18,7 +18,7 @@ async function githubCommand(sock, chatId, message) {
     txt += `✩  *URL* : ${json.html_url}\n`;
     txt += `✩  *Forks* : ${json.forks_count}\n`;
     txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
-    txt += `💥 *Arslan-Ai*`;
+    txt += `💥 *💀💀*`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
